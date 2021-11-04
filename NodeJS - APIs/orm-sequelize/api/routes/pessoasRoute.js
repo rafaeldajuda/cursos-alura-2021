2e36dfd87b164c4d3a3ec71811222dcb0a88f9d6
Rafael <rafaeldajuda@gmail.com>
2021-11-03 22:52:22 -0300

    crud

--- NodeJS - APIs/orm-sequelize/api/routes/pessoasRoute.js
+++ NodeJS - APIs/orm-sequelize/api/routes/pessoasRoute.js
@@ -4,5 +4,9 @@ const PessoaController = require('../controllers/PessoaController');
 const router = Router();
 
 router.get('/pessoas', PessoaController.pegarTodasAsPessoas);
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
+router.post('/pessoas', PessoaController.criaPessoa);
+router.put('/pessoas/:id', PessoaController.atualizaPessoa);
+router.delete('/pessoas/:id', PessoaController.apagaPessoa);
 
 module.exports = router;
